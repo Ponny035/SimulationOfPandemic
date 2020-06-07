ed1746be1e39acb0a070b335944c645b334bbf76
Drive <kr.sannarong@gmail.com>
2020-06-07 19:44:43 +0700

    [Edit] method description
    edit method description

--- Simulation Of Pandemic/ImmunityInformation.java
+++ Simulation Of Pandemic/ImmunityInformation.java
@@ -8,10 +8,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ImmunityInformation extends Information
 {
-    /**
-     * Act - do whatever the ImmunityInformation wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public ImmunityInformation () {
         super("Immunity", ""+SimulationWorld.immune);
     }
